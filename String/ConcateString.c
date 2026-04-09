940fb53de9726aaae4cca345feaf6b510e964e46
Sumit Patil <sumitpatil17020@gmail.com>
2026-04-09 21:38:23 +0530

    Update ConcateString.c

--- String/ConcateString.c
+++ String/ConcateString.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<string.h>
+// strcat() = This function is use to concatenates to two string :
+void fun(char str1[],char str2[]){
+    strcat(str1,str2);
+    printf("After using strcat function valur :%s",str1);
+    //prin("value of str1 is:%s",str1);
+}
+int main(){
+    char str1[]="Hello";
+    char str2[]="Word";
+    fun(str1,str2);
+    return 0;
+}
