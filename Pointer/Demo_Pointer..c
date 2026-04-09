a1b68068c645fe1146a139bf06b7b5e8c1f7ddc9
Sumit Patil <sumitpatil17020@gmail.com>
2026-04-09 18:24:45 +0530

    Update Demo_Pointer..c

--- Pointer/Demo_Pointer..c
+++ Pointer/Demo_Pointer..c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a=15;
+    int *p=&a;
+    //int b=*p;
+    printf("Address Of A Is : %p\n",p);
+    printf("%p\n",&a);
+    printf("%p",&p);
 
+    return 0;
+}
